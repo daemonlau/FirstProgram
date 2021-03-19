60d4ce9b1b08792a8c03c4dfb31a22044511e8ee
daemonlau <daemon.lau@gmail.com>
2021-03-20 05:43:33 +0800

    Java and Kotlin variable,data type.

--- src/main/java/com/liu/Hello.java
+++ src/main/java/com/liu/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
 //        System.out.println("Hello world");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2=19;
+        char A = '天';
+        Character B ='C';
+        float weight = 66.6f;
+        byte c = 10;
+        boolean adult = true;
+        String name = "Daemon";
+
+
     }
 }
