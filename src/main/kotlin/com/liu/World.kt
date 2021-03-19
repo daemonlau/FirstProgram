8a6a7631de346ad9d5c3850406c1ba26d34f835b
daemonlau <daemon.lau@gmail.com>
2021-03-19 22:05:56 +0800

    Initial commit

---
+++ src/main/kotlin/com/liu/World.kt
@@ -0,0 +1,15 @@
+package com.liu
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello kotlin")
+
+    }
+}
